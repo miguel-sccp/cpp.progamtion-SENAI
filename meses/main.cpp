061df070642c8be13ef0812dcfbdb298f9ad10e8
Miguel Reis Ribeiro de Almeida <miguel.reis@hotmail.com.br>
2025-08-13 21:24:12 -0300

    Add files via upload

---
+++ meses/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int meses;
+    cout <<"escolha um dos 12 meses no ano"<<endl;
+    cin>>meses;
+    switch(meses){
+
+case 1:cout<<"vc escolheu janeiro";
+break;
+case 2:cout<<"vc escolheu fevereiro";
+break;
+case 3:cout<<"vc escolheu marco";
+break;
+case 4:cout<<"vc escolheu abril";
+break;
+case 5:cout<<"vc escolheu maio";
+break;
+case 6:cout<<"vc escolheu junho";
+break;
+case 7:cout<<"vc escolheu julho";
+break;
+case 8:cout<<"vc escolheu agosto";
+break;
+case 9:cout<<"vc escolheu setembro";
+break;
+case 10:cout<<"vc escolheu outubro";
+break;
+case 11:cout<<"vc escolheu novembro";
+break;
+case 12:cout<<"vc escolheu dezembro";
+break;
+default: cout<<"nn exite esse mes burro";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+           }
+
+}
