af175c4e755b25af2ada4b7bb11c3f6c2f0da9e1
Miguel Reis Ribeiro de Almeida <miguel.reis@hotmail.com.br>
2025-08-19 21:53:32 -0300

    Create pares.cpp

---
+++ pares.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int n1, n2, n3, n4, n5, n6, n7, n8, n9, n10;
+cout << "Digite 10 numeros:" << endl;
+cin >> n1 >> n2 >> n3 >> n4 >> n5 >> n6 >> n7 >> n8 >> n9 >> n10;
+if (n1 % 2 == 0) cout << n1;
+if (n2 % 2 == 0) cout << n2 <<endl;
+if (n3 % 2 == 0) cout << n3 <<endl ;
+if (n4 % 2 == 0) cout << n4 << endl;
+if (n5 % 2 == 0) cout << n5 << endl;
+if (n6 % 2 == 0) cout << n6 << endl;
+if (n7 % 2 == 0) cout << n7 << endl;
+if (n8 % 2 == 0) cout << n8 << endl;
+if (n9 % 2 == 0) cout << n9 <<endl;
+if (n10 % 2 == 0) cout << n10<< endl;}
