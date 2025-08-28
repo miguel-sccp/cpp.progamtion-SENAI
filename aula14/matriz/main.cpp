1486c3bea6905a9ef7c2272b681b3efb3078a3d6
Miguel Reis Ribeiro de Almeida <miguel.reis@hotmail.com.br>
2025-08-27 21:52:28 -0300

    Add files via upload

---
+++ aula14/matriz/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int matrix [3][3];
+
+matrix[0][0]=1;
+matrix[0][1]=2;
+matrix[0][2]=3;
+matrix[1][0]=4;
+matrix[1][1]=5;
+matrix[1][2]=6;
+matrix[2][0]=7;
+matrix[2][1]=8;
+matrix[2][2]=9;
+cout<<matrix[0][0]<<" "<<matrix[0][1]<<" "<<matrix[0][2]<<endl;
+cout<<matrix[1][0]<<" "<<matrix[1][1]<<" "<<matrix[1][2]<<endl;
+cout<<matrix[2][0]<<" "<<matrix[2][1]<<" "<<matrix[2][2]<<endl;
+return 0;
+}
